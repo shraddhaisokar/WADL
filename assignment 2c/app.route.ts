2d6c3988a5dc190e2bd90f21dbdf5e7b0d88c06d
shraddhaisokar <161551060+shraddhaisokar@users.noreply.github.com>
2025-04-22 12:33:19 +0530

    Create app.route.ts

---
+++ assignment 2c/app.route.ts
@@ -0,0 +1,23 @@
+import { Routes } from '@angular/router';
+import { authGuard } from './guards/auth.guard';
+
+export const routes: Routes = [
+  {
+    path: 'register',
+    loadComponent: () => import('./components/register/register.component').then(m => m.RegisterComponent)
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./components/login/login.component').then(m => m.LoginComponent)
+  },
+  {
+    path: 'profile',
+    loadComponent: () => import('./components/profile/profile.component').then(m => m.ProfileComponent),
+    canActivate: [authGuard]
+  },
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  }
+];
